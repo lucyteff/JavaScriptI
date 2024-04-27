1cb2a505f0ea9d81638746a3de1610419005871b
lucyteff <luciana.pecora@al.infnet.edu.br>
2024-04-26 21:34:22 -0300

    Funcao de calculo de salario liquido

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,12 +1,19 @@
 //input
-anoNascimento=2001
-nome = "Lucy"
+anoNascimento = parseInt(prompt("Informe seu ano de nascimento"))
+nome = prompt("Informe seu nome")
+salarioBase = parseFloat(prompt("Informe o salário base"))
+gratificacao = parseFloat(prompt("Informe a gratificação"))
+bonus = parseFloat(prompt("Informe o bônus"))
+desconto = parseFloat(prompt("Informe o desconto"))
+salarioLiquido = 0
 
 //process
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
-mensagem = "Sou " + nome + " e tenho " + idade + " anos "
+salarioLiquido = salarioBase + gratificacao + bonus - desconto
+mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido
 
+ 
 //output
 alert(mensagem)
