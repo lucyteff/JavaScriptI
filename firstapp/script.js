30ddb902c74741768a38c8cb1b524f43a1f7e22e
lucyteff <luciana.pecora@al.infnet.edu.br>
2024-05-10 20:54:12 -0300

    Fixed

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,3 +1,8 @@
+//constants
+const SALARIO_ATE_20 = 1000
+const SALARIO_ACIMA_20 = 2000
+
+
 //input
 anoNascimento = parseInt(prompt("Informe seu ano de nascimento"))
 nome = prompt("Informe seu nome")
@@ -6,11 +11,15 @@ gratificacao = parseFloat(prompt("Informe a gratificação"))
 bonus = parseFloat(prompt("Informe o bônus"))
 desconto = parseFloat(prompt("Informe o desconto"))
 salarioLiquido = 0
+adicional = 0
 
 //process
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
+
+//IF:
+adicional = SALARIO_ATE_20
 salarioLiquido = salarioBase + gratificacao + bonus - desconto
 mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido
 
